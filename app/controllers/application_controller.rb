56310428c046776049bd706669f15fcbc28159c9
Shun712 <sh705vi@gmail.com>
2022-01-22 00:44:06 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # フラッシュメッセージのキーを許可する記述
+  # これでBootstrapに対応した success info warning danger 4つのキーが使用できる
+  add_flash_types :success, :info, :warning, :danger
 end
