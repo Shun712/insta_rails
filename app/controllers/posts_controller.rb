245015da1e30bd1a3e9a42ecfe44423c33fc5637
Shun712 <sh705vi@gmail.com>
2022-01-23 07:03:06 +0900

    [Modify]タイポを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :required_login, only: %i[new create edit update destroy]
+  before_action :require_login, only: %i[new create edit update destroy]
   def index
     # N + 1問題の対応
     @posts = Post.all.includes(:user).order(created_at: :desc)
