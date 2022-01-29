94efbbeefce062fdc535cec5635838bcf1a252ec
Shun712 <sh705vi@gmail.com>
2022-01-30 01:40:41 +0900

    [Modify]未ログイン時ページングが正常に機能しない問題を修正

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,5 +1,5 @@
 puts 'Start inserting seed "posts" ...'
-User.limit(10).each do |user|
+User.limit(30).each do |user|
   # Lorem Picsumというgemを使用。画像を返してくれる便利サービス。
   # remote_images_urls: というカラムは、「https://~~~.jpg」のような外部から取ってきた画像を設定する。
   # 「remote_カラム名_url」とすれば使える。
