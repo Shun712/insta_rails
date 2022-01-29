cb82b4692540bd8a569ef88e789d9ebff0f9f3ac
Shun712 <sh705vi@gmail.com>
2022-01-30 01:26:46 +0900

    [Modify]タイポ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,7 +68,7 @@ class User < ApplicationRecord
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
 
-  def follow?(other_user)
+  def following?(other_user)
     following.include?(other_user)
   end
 
