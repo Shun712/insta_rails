e4e620567e4fd88c98edc840e36a3a516370fcbd
Shun712 <sh705vi@gmail.com>
2022-02-09 10:01:30 +0900

    [Add]コードの説明を追記

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+# コンパイルは、CoffeeScriptとか、SCSSとか、ブラウザが直接読めない形式のファイルを、JSやCSSに変換する
+# プリコンパイルは、rails s　する前に上記をしてくれる。
+# アセットは簡単に言えば「ブラウザが読むHTML以外のファイル」
+# application.jsとapplication.cssは設定せず、プリコンパイルの対象になっている
+# それ以外は、指定しなければならない
 Rails.application.config.assets.precompile += %w[mypage.js mypage.css]
