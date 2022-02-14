23e34e3ae45b19c7f76b748246fa887d971ceca9
Shun712 <sh705vi@gmail.com>
2022-02-14 17:01:59 +0900

    [Modify]タイポ修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     # ビューのクエリパラメータから:followed_idを取得
     @user = User.find(params[:followed_id])
-    UserMailer.with(user_from: current_user, user_to: @user).follow.deliver_later if current_user.follow(@user)
+    UserMailer.with(user_from: current_user, user_to: @user).follow.deliver_now if current_user.follow(@user)
   end
 
   def destroy
