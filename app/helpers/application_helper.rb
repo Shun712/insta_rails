651a6500f3401489db8d1e343482db1bc5709268
Shun712 <sh705vi@gmail.com>
2022-02-16 15:36:21 +0900

    [Modify]タイポ修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
-  def default_meta_tag
+  def default_meta_tags
     {
       site: Settings.meta.site,
       reverse: true,
       title: Settings.meta.title,
       description: Settings.meta.description,
       keywords: Settings.meta.keywords,
-      canonical: request.original.url,
+      canonical: request.original_url,
       og: {
         title: :full_title,
         type: Settings.meta.og.type,
         url: request.original_url,
         image: image_url(Settings.meta.og.image_path),
         site_name: :site,
-        desciption: :desciption,
+        description: :description,
         locale: 'ja_JP'
       },
       twitter: {
